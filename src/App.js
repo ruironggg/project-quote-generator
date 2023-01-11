accd84e7d9f74785eee1b80d2dfa991e52a05df6
ruirong <rune_rui_rong@hotmail.com>
2023-01-11 15:10:41 -0500

    completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
+
+import { useState } from 'react';
 import './App.css';
+import Axios from "axios";
 
 function App() {
+
+  const [ advice, setAdvice ] = useState("");
+
+  //fetch the data from the api
+  const fetchData = () => {
+    Axios.get('https://api.adviceslip.com/advice').then( (res) => {
+      setAdvice(res.data.slip.advice);
+    });
+  };
+
+
+//return it
   return (
     <div className="App">
-      
+      <div className='card'>
+        <h1 className='advices'> {advice} </h1>
+        <button className='button' onClick={fetchData}> <span> Refresh Advice </span> </button>
+      </div>
     </div>
   );
-}
+
+};
 
 export default App;
